06ae2b8b4a6b108d28dd3223548ec6f832ff3f98
Braxxian <killarneypc1@gmail.com>
2023-09-22 16:13:57 +0100

    add game title

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pyfiglet import figlet_format
+from termcolor import colored, cprint
+import colorama
+from colorama import Fore, Back
+colorama.init(autoreset=True)
+color = "green"
+title = (figlet_format("Welcome", font="doom"))
+cprint(title, color)
+
+print(Back.LIGHTGREEN_EX + Fore.BLACK +
+      "*------------------------*".center(40, ' '))
+print(Back.LIGHTGREEN_EX + Fore.BLACK +
+      "Lost in the 'Haunted Wood'".center(40, ' '))
+print(Back.LIGHTGREEN_EX + Fore.BLACK +
+      "*------------------------*".center(40, ' '))
