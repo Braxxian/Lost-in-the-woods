55fffe8ba9294f3796fcace5a4c6cb9d9b77ee0e
Braxxian <killarneypc1@gmail.com>
2023-09-25 11:37:41 +0100

    move adventure text to seperate file

---
+++ game_text.py
@@ -0,0 +1,13 @@
+adventure_text = {
+    "intro_choiceA":
+        """You know you've been here before, in a dream maybe ?
+        The dark wood rustles in the night wind
+        rubbing your eyes, you see two paths, dimly lit in the moonlight
+        Will you go left or right?""",
+
+    "intro_choiceB":
+        """A wise choice, few who enter the wood
+        ever make it out alive or with their sanity intact
+        Goodbye!"""
+
+}
--- run.py
+++ run.py
@@ -4,22 +4,10 @@ from pyfiglet import figlet_format
 from termcolor import cprint
 import colorama
 from colorama import Fore, Back
-
+from game_text import adventure_text
 colorama.init(autoreset=True)
 
-adventure_text = {
-    "intro_choiceA":
-        """You know you've been here before, in a dream maybe ?
-        The dark wood rustles in the night wind
-        rubbing your eyes, you see two paths, dimly lit in the moonlight
-        Will you go left or right?""",
 
-    "intro_choiceB":
-        """A wise choice, few who enter the wood
-        ever make it out alive or with their sanity intact
-        Goodbye!"""
-
-}
 # center text
 
 
